e734d5f11b57649150da402957aefac7aca44af0
Vasu007-tech <vasuagrawal1903@gmail.com>
2026-09-03 23:01:40 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.3%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,6 +11,16 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         vector<vector<int>> dp(m,vector<int>(m,INT_MAX));
-        return func(0,0,triangle,dp);
+        for(int i=0;i<m;i++){
+            dp[m-1][i]=triangle[m-1][i];
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                 int d=triangle[i][j]+dp[i+1][j];
+        int dg = triangle[i][j]+dp[i+1][j+1];
+      dp[i][j]=min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
